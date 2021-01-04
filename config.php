4fc84f6697e252e8067aa5d3ae95fd19087f56d6
nikunjprajapati77 <36290753+nikunjprajapati77@users.noreply.github.com>
2021-01-04 17:10:41 +0530

    First-commit

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'vendor/autoload.php';
+require_once "class-db.php";
+ 
+define('CLIENT_ID', 't4us9wtESkm_sLpOGIXrNg');
+define('CLIENT_SECRET', 'e4caZFNxuX3pHn8VOCC1c8ZY0frRXW6N');
+define('REDIRECT_URI', 'https://af681f0a7ff4.ngrok.io/zoom/callback.php');
